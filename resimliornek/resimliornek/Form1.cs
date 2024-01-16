c6211d9be73ad688b91b632e9f3d5ca6f82bb1c1
sumeyyedemirci2023 <145452558+sumeyyedemirci2023@users.noreply.github.com>
2024-01-16 10:15:39 +0300

    Add files via upload

---
+++ resimliornek/resimliornek/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace resimliornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Bitmap>resimler =new List<Bitmap>();
+        int sayac = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimler.Add(Properties.Resources.resim1);
+            resimler.Add(Properties.Resources.resim2);
+            resimler.Add(Properties.Resources.resim3);
+            resimler.Add(Properties.Resources.resim4);
+            resimler.Add(Properties.Resources.resim5);
+           
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            sayac=sayac % resimler.Count;
+            pictureBox1.Image = resimler[sayac];
+        }
+    }
+}
